517a426abd7ba09e779352c19d0c9310b5ccc925
Kirill Kotov <krlktv@mail.ru>
2021-11-22 17:28:13 +0300

    feat: implement search & filter

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -35,6 +35,8 @@ class App extends Component {
 					id: 3,
 				},
 			],
+			term: '',
+			filter: 'all',
 		};
 		this.maxId = 4;
 	}
@@ -74,21 +76,52 @@ class App extends Component {
 		}));
 	};
 
+	searchEmp = (items, term) => {
+		if (term.length === 0) {
+			return items;
+		}
+
+		return items.filter((item) => {
+			return item.name.toLowerCase().indexOf(term.toLowerCase()) > -1;
+		});
+	};
+
+	onUpdateSearch = (term) => {
+		this.setState({ term });
+	};
+
+	filterPost = (items, filter) => {
+		switch (filter) {
+			case 'rise':
+				return items.filter((item) => item.rise);
+			case '>1000':
+				return items.filter((item) => item.salary > 1000);
+			default:
+				return items;
+		}
+	};
+
+	onFilterSelect = (filter) => {
+		this.setState({ filter });
+	};
+
 	render() {
-		const employees = this.state.data.length;
-		const increased = this.state.data.filter((item) => item.increase).length;
+		const { data, term, filter } = this.state;
+		const employees = data.length;
+		const increased = data.filter((item) => item.increase).length;
+		const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
 		return (
 			<div className="app">
 				<AppInfo employees={employees} increased={increased} />
 
 				<div className="search-panel">
-					<SearchPanel />
-					<AppFilter />
+					<SearchPanel onUpdateSearch={this.onUpdateSearch} />
+					<AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
 				</div>
 
 				<EmployeesList
-					data={this.state.data}
+					data={visibleData}
 					onDelete={this.deleteItem}
 					onToggleProp={this.onToggleProp}
 				/>
